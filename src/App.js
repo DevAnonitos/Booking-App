d4266a49842726d47737c61e36fa1bfafe69ab7f
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-09 01:54:16 +0700

    update-components&pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+// React & Router V6
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// import components
+import Home from "./pages/home/Home";
+import Hotel from './pages/hotel/Hotel';
+import List from './pages/list/List';
 
 function App() {
+
   return (
-    <div className="App">
-      Hello
-    </div>
+    // Route page 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
