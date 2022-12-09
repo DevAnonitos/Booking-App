5e1d6fe36564f0c6d49c36138500f2abbf7cfd32
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-10 00:17:56 +0700

    update-header#2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { faBed } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <div className="headerList">
+        <div className="headerItems">
+          <FontAwesomeIcon icon={faBed} />
+          <span>Stay</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className='navContainer'>
-            <span className="logo">TravelBooking</span>
-            <div className="navItems">
-                <button className="navButton">Register</button>
-                <button className="navButton">Login</button>
-            </div>
+          <span className="logo">TravelBooking</span>
+          <div className="navItems">
+              <button className="navButton">Register</button>
+              <button className="navButton">Login</button>
+          </div>
         </div>
     </div>
   )
