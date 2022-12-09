dc0632f06171b4f9e964946d9ac63697395e6ed0
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-09 23:56:49 +0700

    update-header#1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
+import Header from '../../components/header/Header';
 import "./home.css";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   )
 }
