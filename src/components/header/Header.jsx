a0d016cdb4a2663a4e97a3799254ea2b3329adf0
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-10 17:23:20 +0700

    update-header#3

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,28 +6,45 @@ import "./header.css";
 const Header = () => {
   return (
     <div className='header'>
-      <div className="headerList">
-        <div className="headerItems">
-          <FontAwesomeIcon icon={faBed} />
-          <span>Stay</span>
-        </div>
-        <div className="headerListItem">
-            <FontAwesomeIcon icon={faPlane} />
-            <span>Flights</span>
-          </div>
-          <div className="headerListItem">
-            <FontAwesomeIcon icon={faCar} />
-            <span>Car rentals</span>
-          </div>
+      <div className="headerContainer">
+        <div className="headerList">
+          {/* =====================HEADER ITEMS=============== */}
           <div className="headerListItem">
             <FontAwesomeIcon icon={faBed} />
-            <span>Attractions</span>
+            <span>Stay</span>
           </div>
           <div className="headerListItem">
-            <FontAwesomeIcon icon={faTaxi} />
-            <span>Airport taxis</span>
-          </div>
+              <FontAwesomeIcon icon={faPlane} />
+              <span>Flights</span>
+            </div>
+            <div className="headerListItem">
+              <FontAwesomeIcon icon={faCar} />
+              <span>Car rentals</span>
+            </div>
+            <div className="headerListItem">
+              <FontAwesomeIcon icon={faBed} />
+              <span>Attractions</span>
+            </div>
+            <div className="headerListItem">
+              <FontAwesomeIcon icon={faTaxi} />
+              <span>Airport taxis</span>
+            </div>
+        </div>
+        <h1 className="headerTitle">
+          A lifetime of discounts? It's Genius.
+        </h1>
+        <p className="headerDesc">
+          Get rewarded for your travels – unlock instant savings of 10% or
+          more with a free Lamabooking account
+        </p>
+        <button className="headerBtn">
+          Sign in / Register
+        </button>
+        <div className="headerSearch">
+          
+        </div>
       </div>
+      
     </div>
   )
 }
