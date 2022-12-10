6341eea6e9e03fb831d6503e0a8569ed187d335e
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-10 16:25:13 +0700

    update-src#3

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { faBed } from "@fortawesome/free-solid-svg-icons";
+import { faBed, faPlane, faCar, faTaxi } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./header.css";
 
@@ -11,6 +11,22 @@ const Header = () => {
           <FontAwesomeIcon icon={faBed} />
           <span>Stay</span>
         </div>
+        <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport taxis</span>
+          </div>
       </div>
     </div>
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import "./navbar.css";
 
+
 const Navbar = () => {
+
   return (
     <div className='navbar'>
         <div className='navContainer'>
           <span className="logo">TravelBooking</span>
           <div className="navItems">
-              <button className="navButton">Register</button>
-              <button className="navButton">Login</button>
+            <FontAwesomeIcon icon={faBars} className="btnMenu" />
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
           </div>
         </div>
     </div>
