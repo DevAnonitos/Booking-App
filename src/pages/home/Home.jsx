4b48bb9275bccb58b561d5dbb88d9c50d0a026a2
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-13 17:08:44 +0700

    update-featured#2

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,6 +14,7 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
       </div>
     </div>
   )
