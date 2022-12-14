539ec28babd1a48d292de70b1fda9156c00254f2
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-14 22:49:19 +0700

    update-src

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
 import Featured from '../../components/featured/Featured';
 import PropertyList from '../../components/propertyList/PropertyList';
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
 
 import "./home.css";
 
@@ -17,6 +18,8 @@ const Home = () => {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList/>
+        <h1 className="homeTitle">Home guests love</h1>
+        <FeaturedProperties />
       </div>
     </div>
   )
