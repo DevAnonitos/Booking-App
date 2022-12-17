93a5a104f4b2113a479bebb19aabd201389e6d75
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-17 09:51:33 +0700

    update-MailList

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,9 +4,9 @@ import Header from '../../components/header/Header';
 import Featured from '../../components/featured/Featured';
 import PropertyList from '../../components/propertyList/PropertyList';
 import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
+import MailList from '../../components/mailList/MailList';
 
 import "./home.css";
-import MailList from '../../components/mailList/MailList';
 
 
 const Home = () => {
@@ -17,9 +17,13 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured />
-        <h1 className="homeTitle">Browse by property type</h1>
+        <h1 className="homeTitle">
+          Browse by property type
+        </h1>
         <PropertyList/>
-        <h1 className="homeTitle">Home guests love</h1>
+        <h1 className="homeTitle">
+          Home guests love
+        </h1>
         <FeaturedProperties />
         <MailList />
       </div>
