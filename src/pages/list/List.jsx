6018c7cf119b36e5a2409158c0a6ad3697f0c75a
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-18 09:55:59 +0700

    update-listSearch

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -9,6 +9,24 @@ const List = () => {
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="listTitle">Search</h1>
+            <div className="listItems">
+              <label>Destination</label>
+              <input type="text" />
+            </div>
+            <div className="listItems">
+              <label>Check-in Date</label>
+              
+            </div>
+          </div>
+          <div className="listResult">
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
